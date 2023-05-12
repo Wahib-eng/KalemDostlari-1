b2ff1829f25afbbb01469775c146268ce55ce648
WAHIB MOQBEL <75446834+Wahib-eng@users.noreply.github.com>
2023-05-12 16:40:51 +0300

    Update sign_up_do.php

--- kalemDostlari/sign_up_do.php
+++ kalemDostlari/sign_up_do.php
@@ -1,24 +1,28 @@
 <?php
-
 include_once "connection.php";
 
-$newcus_name = $_POST["name"];
-$newcus_surname = $_POST["surname"];
-$newcus_tell = $_POST["tell"];
-$newcus_adress = $_POST["adress"];
-$newcus_username = $_POST["username"];
-$newcus_password = $_POST["password"];
+if (isset($_POST["name"]) && isset($_POST["surname"]) && isset($_POST["age"]) && isset($_POST["departman"]) && isset($_POST["school"]) && isset($_POST["classLevel"]) && isset($_POST["username"]) && isset($_POST["passwords"])) {
+    $newsut_name = $_POST["name"];
+    $newsut_surname = $_POST["surname"];
+    $newsut_age = $_POST["age"];
+    $newsut_departman = $_POST["departman"];
+    $newsut_school = $_POST["school"];
+    $newsut_classLevel = $_POST["classLevel"];
+    $newsut_username = $_POST["username"];
+    $newsut_password = $_POST["passwords"];
+    
+    $q = mysqli_query($db_con, "INSERT INTO student (name, surname, age, departman, school, classLevel, username, passwords) VALUES ('".$newsut_name."','".$newsut_surname."','".$newsut_age."','".$newsut_departman."','".$newsut_school."','".$newsut_classLevel."','".$newsut_username."','".$newsut_password."')");
+  
+    if (!$q) {
+        echo mysqli_error($db_con);
+    }
 
-if( isset($_POST["name"]) && isset($_POST["surname"])
-&& $_POST["tell"] && $_POST["adress"] && $_POST["username"] && $_POST["password"]
-){
-    $q = mysqli_query($db_con, "INSERT INTO customer (names,surname,tell,adress,username,passwords) VALUES ('".$newcus_name."','".$newcus_surname."','".$newcus_tell ."','".$newcus_adress."','".$newcus_username."','".$newcus_password."')");
-    echo mysqli_error($db_con);
-
-    header("location: index.php");
-}else{
-    header("location: sign_up.php");
+    header("location:index.php");
+} else {
+    header("location: ogrenci_sign_up.php");
 }
+
+
 
 
 ?>
