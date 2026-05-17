3383b18321271e12896f53e753d0c55b191a49ee
WAHIB MOQBEL <75446834+Wahib-eng@users.noreply.github.com>
2026-05-17 11:32:17 +0300

    Update navbar.php

--- kalemDostlari/includes/navbar.php
+++ kalemDostlari/includes/navbar.php
@@ -1,1 +1,53 @@
+```php
+<nav class="navbar navbar-expand-lg fixed-top custom-navbar">
+
+    <div class="container">
+
+        <a class="navbar-brand" href="index.php">
+            Kalem Dostları
+        </a>
+
+        <button class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav">
+
+            <span class="navbar-toggler-icon"></span>
+
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+
+            <ul class="navbar-nav ms-auto">
+
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Anasayfa</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="pages/hakkimizda.php">Hakkımızda</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="pages/nasilCalisir.php">Nasıl Çalışır?</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="pages/iletisim.php">İletişim</a>
+                </li>
+
+                <li class="nav-item ms-3">
+                    <a class="btn btn-main" href="#">
+                        Giriş Yap
+                    </a>
+                </li>
+
+            </ul>
+
+        </div>
+
+    </div>
+
+</nav>
+```
 
