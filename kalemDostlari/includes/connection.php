29a1ddc59e392e948c427f29ff18a308d38cd275
WAHIB MOQBEL <75446834+Wahib-eng@users.noreply.github.com>
2026-05-17 11:43:29 +0300

    Update connection.php

--- kalemDostlari/includes/connection.php
+++ kalemDostlari/includes/connection.php
@@ -1,1 +1,28 @@
+```php id="x5yzgk"
+<?php
+
+$host = "localhost";
+$dbname = "kalem_dostlari";
+$username = "root";
+$password = "";
+
+try {
+
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8",
+        $username,
+        $password
+    );
+
+    $pdo->setAttribute(
+        PDO::ATTR_ERRMODE,
+        PDO::ERRMODE_EXCEPTION
+    );
+
+} catch(PDOException $e) {
+
+    die("Connection failed: " . $e->getMessage());
+}
+?>
+```
 
