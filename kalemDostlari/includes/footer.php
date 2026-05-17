46e096acbcb63ec0abb12026b57b8e3735e9db22
WAHIB MOQBEL <75446834+Wahib-eng@users.noreply.github.com>
2026-05-17 11:32:36 +0300

    Add footer section with social icons and copyright

--- kalemDostlari/includes/footer.php
+++ kalemDostlari/includes/footer.php
@@ -1,1 +1,30 @@
+```php
+<footer class="footer">
+
+    <div class="container text-center">
+
+        <h3>Kalem Dostları</h3>
+
+        <p>
+            Depremzede öğrencilerin geleceğine destek oluyoruz.
+        </p>
+
+        <div class="social-icons">
+
+            <i class="bi bi-instagram"></i>
+            <i class="bi bi-twitter-x"></i>
+            <i class="bi bi-linkedin"></i>
+
+        </div>
+
+        <hr>
+
+        <p class="copyright">
+            © 2026 Kalem Dostları - Tüm Hakları Saklıdır
+        </p>
+
+    </div>
+
+</footer>
+```
 
